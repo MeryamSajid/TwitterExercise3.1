3b7b7d78f94209c7799146a56b77282e6dbc4021
MeryamSajid <143551946+MeryamSajid@users.noreply.github.com>
2023-09-21 01:57:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Type your tweet: ");
+    Console.ReadLine ();
+  if () {
+    Console.WriteLine ("Posted")}
+
+  else () {
+    Console.WriteLine ("Rejected")
+    }
+
+    
+  }
+  
+}
