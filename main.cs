d9379797cbe03b8a54b63572bb0ec4e8516d9c2b
MeryamSajid <143551946+MeryamSajid@users.noreply.github.com>
2023-09-21 02:21:08 +0000

    update

--- main.cs
+++ main.cs
@@ -2,13 +2,18 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Type your tweet: ");
-    Console.ReadLine ();
-  if () {
-    Console.WriteLine ("Posted")}
 
-  else () {
-    Console.WriteLine ("Rejected")
+    string tweet;
+    
+    Console.WriteLine ("Type your tweet: ");
+    tweet = Console.ReadLine();
+    int tweetLength = tweet.Length;
+        
+  if (tweetLength <= 140 ) {
+    Console.WriteLine ("Posted");
+    
+  } else {
+    Console.WriteLine ("Rejected");
     }
 
     
